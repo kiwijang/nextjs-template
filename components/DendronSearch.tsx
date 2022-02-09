40b554372c25e255a49ae390b20c1acd04c8ebd9
naomi@miniasp.com <naomi@miniasp.com>
2022-02-09 23:34:14 +0800

    修改 search css

--- components/DendronSearch.tsx
+++ components/DendronSearch.tsx
@@ -2,30 +2,30 @@ import {
   NoteIndexProps,
   NoteLookupUtils,
   NoteProps,
-} from "@dendronhq/common-all";
-import { LoadingStatus } from "@dendronhq/common-frontend";
-import { AutoComplete, Alert, Row, Col, Typography } from "antd";
-import React, { useEffect } from "react";
-import { useCombinedDispatch } from "../features";
-import { browserEngineSlice } from "../features/engine";
-import { useFetchFuse } from "../utils/fuse";
-import type Fuse from "fuse.js";
+} from '@dendronhq/common-all';
+import { LoadingStatus } from '@dendronhq/common-frontend';
+import { AutoComplete, Alert, Row, Col, Typography } from 'antd';
+import React, { useEffect } from 'react';
+import { useCombinedDispatch } from '../features';
+import { browserEngineSlice } from '../features/engine';
+import { useFetchFuse } from '../utils/fuse';
+import type Fuse from 'fuse.js';
 import {
   DendronCommonProps,
   DendronPageWithNoteDataProps,
   verifyNoteData,
-} from "../utils/types";
-import DendronSpinner from "./DendronSpinner";
-import { useDendronLookup, useNoteActive, useNoteBodies } from "../utils/hooks";
-import FileTextOutlined from "@ant-design/icons/lib/icons/FileTextOutlined";
-import _ from "lodash";
+} from '../utils/types';
+import DendronSpinner from './DendronSpinner';
+import { useDendronLookup, useNoteActive, useNoteBodies } from '../utils/hooks';
+import FileTextOutlined from '@ant-design/icons/lib/icons/FileTextOutlined';
+import _ from 'lodash';
 
 /** For notes where nothing in the note body matches, only show this many characters for the note body snippet. */
 const MAX_NOTE_SNIPPET_LENGTH = 30;
 /** For each matching part in the note body, show this many characters before and after the matching part in each snippet. */
 const NOTE_SNIPPET_BEFORE_AFTER = 100;
 /** Place this in place of omitted text in between snippet parts. */
-const OMITTED_PART_TEXT = " ... ";
+const OMITTED_PART_TEXT = ' ... ';
 /** How long to wait for before triggering fuse search, in ms. Required for performance reasons since fuse search is expensive. */
 const SEARCH_DELAY = 300;
 
@@ -66,13 +66,13 @@ function DebouncedDendronSearchComponent(props: DendronPageWithNoteDataProps) {
 
 type SearchResults = Fuse.FuseResult<NoteProps>[] | undefined;
 
-type SearchProps = Omit<ReturnType<typeof useFetchFuse>, "fuse"> & {
+type SearchProps = Omit<ReturnType<typeof useFetchFuse>, 'fuse'> & {
   search: SearchFunction | undefined;
 };
 
 enum SearchMode {
-  LOOKUP_MODE = "lookupMode",
-  SEARCH_MODE = "searchMode",
+  LOOKUP_MODE = 'lookupMode',
+  SEARCH_MODE = 'searchMode',
 }
 
 function DendronSearchComponent(
@@ -80,8 +80,9 @@ function DendronSearchComponent(
 ) {
   const { noteIndex, dendronRouter, search, error, loading, notes } = props;
 
-  const [searchResults, setSearchResults] =
-    React.useState<SearchResults>(undefined);
+  const [searchResults, setSearchResults] = React.useState<SearchResults>(
+    undefined
+  );
   const [lookupResults, setLookupResults] = React.useState<NoteIndexProps[]>(
     []
   );
@@ -90,7 +91,7 @@ function DendronSearchComponent(
   const { noteBodies, requestNotes } = useNoteBodies();
   const lookup = useDendronLookup();
   const { noteActive } = useNoteActive(dendronRouter.getActiveNoteId());
-  const initValue = noteActive?.fname || "";
+  const initValue = noteActive?.fname || '';
   const [searchQueryValue, setSearchQueryValue] = React.useState(initValue);
 
   useEffect(() => {
@@ -104,7 +105,7 @@ function DendronSearchComponent(
   }, [requestNotes, searchResults]);
 
   useEffect(() => {
-    if (searchQueryValue?.startsWith("?")) {
+    if (searchQueryValue?.startsWith('?')) {
       setResults(SearchMode.SEARCH_MODE);
     } else {
       setResults(SearchMode.LOOKUP_MODE);
@@ -116,7 +117,7 @@ function DendronSearchComponent(
       return;
     }
     const out =
-      qs === ""
+      qs === ''
         ? NoteLookupUtils.fetchRootResults(notes)
         : lookup?.queryNote({ qs });
     setLookupResults(_.isUndefined(out) ? [] : out);
@@ -148,7 +149,10 @@ function DendronSearchComponent(
 
   return (
     <AutoComplete
-      size="middle"
+      style={{ minWidth: 150, color: 'white', width: 'auto' }}
+      getPopupContainer={() =>
+        document.getElementById('my-search') as HTMLElement
+      }
       allowClear
       value={searchQueryValue}
       // @ts-ignore
@@ -162,9 +166,9 @@ function DendronSearchComponent(
         dispatch(
           browserEngineSlice.actions.setLoadingStatus(LoadingStatus.PENDING)
         );
-        setSearchQueryValue("");
+        setSearchQueryValue('');
       }}
-      placeholder={loading ? "Loading Search" : "Full text search ? xxx"}
+      placeholder={loading ? 'Loading Search' : 'Full text search ? xxx'}
     >
       {results === SearchMode.SEARCH_MODE
         ? searchResults?.map(({ item: note, matches }) => {
@@ -172,8 +176,8 @@ function DendronSearchComponent(
               <AutoComplete.Option key={note.id} value={note.fname}>
                 <Row justify="center" align="middle">
                   <Col xs={0} md={1}>
-                    <div style={{ position: "relative", top: -12, left: 0 }}>
-                      <FileTextOutlined style={{ color: "#B18C67" }} />
+                    <div style={{ position: 'relative', top: -12, left: 0 }}>
+                      <FileTextOutlined style={{ color: '#B18C67' }} />
                     </div>
                   </Col>
                   <Col
@@ -181,7 +185,7 @@ function DendronSearchComponent(
                     sm={24}
                     md={11}
                     lg={11}
-                    style={{ borderRight: "1px solid #d4dadf" }}
+                    style={{ borderRight: '1px solid #d4dadf' }}
                   >
                     <Row>
                       <Typography.Text>
@@ -231,7 +235,7 @@ function DendronSearchComponent(
 
 /** Removes repeating newlines from the text. */
 function cleanWhitespace(text: string) {
-  return _.trim(text, "\n").replaceAll(/\n\n/g, "");
+  return _.trim(text, '\n').replaceAll(/\n\n/g, '');
 }
 
 /** For a fuse.js match on a note, renders snippets from the note **body** with the matched parts highlighted.  */
@@ -243,13 +247,13 @@ function MatchBody(props: {
   const body = props.noteBodies[props.id];
   // May happen when note bodies are still loading
   if (_.isUndefined(body))
-    return <span style={{ fontWeight: "lighter" }}>{OMITTED_PART_TEXT}</span>;
+    return <span style={{ fontWeight: 'lighter' }}>{OMITTED_PART_TEXT}</span>;
   // Map happen if the note only matches with title
   if (_.isUndefined(props.matches))
     return <>{body.slice(undefined, MAX_NOTE_SNIPPET_LENGTH)}</>;
   const bodyMatches = props.matches
     // Extract the ranges of body matches
-    .filter((match) => match.key === "body")
+    .filter((match) => match.key === 'body')
     .flatMap((match) => match.indices)
     // Sort from longest range to the shortest
     .sort(([lStart, lEnd], [rStart, rEnd]) => rEnd - rStart - (lEnd - lStart));
@@ -271,7 +275,7 @@ function MatchBody(props: {
   renderedBody.push(
     <span
       key={`${props.id}-${startIndex}-${endIndex}`}
-      style={{ fontWeight: "bold" }}
+      style={{ fontWeight: 'bold' }}
     >
       {cleanWhitespace(body.slice(startIndex, endIndex + 1))}
     </span>
@@ -289,10 +293,10 @@ function MatchBody(props: {
   return (
     <div
       style={{
-        wordWrap: "break-word",
-        whiteSpace: "pre-wrap",
-        fontSize: "0.8rem",
-        marginLeft: "8px",
+        wordWrap: 'break-word',
+        whiteSpace: 'pre-wrap',
+        fontSize: '0.8rem',
+        marginLeft: '8px',
       }}
     >
       {renderedBody}
@@ -309,7 +313,7 @@ function OmittedText(props: { after: number; before: number; body: string }) {
     return <>{cleanWhitespace(body.slice(before, after))}</>;
   } else {
     // If the gap is bigger, then show the "..." text
-    return <span style={{ fontWeight: "lighter" }}>{OMITTED_PART_TEXT}</span>;
+    return <span style={{ fontWeight: 'lighter' }}>{OMITTED_PART_TEXT}</span>;
   }
 }
 
@@ -317,11 +321,11 @@ function OmittedText(props: { after: number; before: number; body: string }) {
 // Recursively builds JSX output adding `<mark>` tags around matches
 const highlight: (
   i: number,
-  indices?: Fuse.FuseResultMatch["indices"],
+  indices?: Fuse.FuseResultMatch['indices'],
   value?: string
 ) => JSX.Element | null = (
   i: number,
-  indices?: Fuse.FuseResultMatch["indices"],
+  indices?: Fuse.FuseResultMatch['indices'],
   value?: string
 ) => {
   const pair = indices?.[indices.length - i];
@@ -331,7 +335,7 @@ const highlight: (
   ) : (
     <>
       {highlight(i + 1, indices, value?.substring(0, pair[0]))}
-      <span style={{ fontWeight: "bolder" }}>
+      <span style={{ fontWeight: 'bolder' }}>
         {value?.substring(pair[0], pair[1] + 1)}
       </span>
       {value?.substring(pair[1] + 1)}
@@ -352,7 +356,7 @@ const TitleHighlight = ({
   title: string;
 }): JSX.Element | null => {
   const matches =
-    typeof hit.item === "string"
+    typeof hit.item === 'string'
       ? hit.matches?.[0]
       : hit.matches?.find((m) => m.key === attribute);
 
