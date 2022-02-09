56ab662da69b31d4e49ef7da3b93e4f7d485a762
naomi@miniasp.com <naomi@miniasp.com>
2022-02-10 00:02:07 +0800

    更新 disqus 留言板 判斷

--- components/Disqus.tsx
+++ components/Disqus.tsx
@@ -1,8 +1,8 @@
-import { verifyEngineSliceState } from "@dendronhq/common-frontend";
-import { Col, Divider } from "antd";
-import { DiscussionEmbed } from "disqus-react";
-import { useEngineAppSelector } from "../features/engine/hooks";
-import { getRootUrl } from "../utils/links";
+import { verifyEngineSliceState } from '@dendronhq/common-frontend';
+import { Col, Divider } from 'antd';
+import { DiscussionEmbed } from 'disqus-react';
+import { useEngineAppSelector } from '../features/engine/hooks';
+import { getRootUrl } from '../utils/links';
 
 const DisqusComments = (noteObj: any) => {
   const engine = useEngineAppSelector((state) => state.engine);
@@ -11,18 +11,38 @@ const DisqusComments = (noteObj: any) => {
   }
   const url = getRootUrl(engine.config.site);
   const { note } = noteObj;
-  // console.log(note);
+  console.log(note);
+  if (note.custom.status === 'w') {
+    return (
+      <span style={{ fontSize: '14px', color: 'lightgrey' }}>
+        <span
+          style={{
+            display: 'inline-block',
+            width: '8px',
+            height: '8px',
+            backgroundColor: 'pink',
+            marginLeft: '10px',
+            marginRight: '4px',
+            borderRadius: '10px',
+          }}
+        ></span>
+        pending notes (這篇文章還沒寫完)
+      </span>
+    );
+  }
   if (
     !note.id ||
-    note.id === "root" ||
-    note.parent === "root" ||
+    note.id === 'root' ||
+    note.parent === 'root' ||
     note.custom?.stub === true ||
-    note.id === "03vN8OL5CrZW9GX2NX7oS"
+    note.id === '03vN8OL5CrZW9GX2NX7oS' ||
+    note.fname.split('.')[0] === 'tags' ||
+    note.fname.split('.')[0] === '0drops'
   ) {
     return <></>;
   }
 
-  const disqusShortname = "kiwijang";
+  const disqusShortname = 'kiwijang';
   const disqusConfig = {
     url: `${url}/notes/${note.id}`,
     identifier: note.id, // Single post id
